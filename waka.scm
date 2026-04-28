37330540c5c1dfd41befcefab87c65ce99a4ec52
Vic <vicxd@pm.me>
2026-04-28 22:30:43 +0200

    set up initial vars and fix cursor position pulling

--- waka.scm
+++ waka.scm
@@ -1,26 +1,35 @@
 (require "helix/editor.scm")
+(require "helix/configuration.scm")
+(require "helix/components.scm")
+(require "helix/commands.scm")
 (require "steel/result")
 (require-builtin steel/process)
 (require-builtin steel/strings)
 (require-builtin steel/time)
 
+
+(define version "v0.26.04dev")
+
+
 (define cursor_row #f)
 (define cursor_col #f)
 (define home "")
+(define message "")
 
-(define (get-cursor-pos-row-col)
-    (match (current-cursor)
-        [(list pos kind)
-            ]
-    )
-)
+(define (get-cursor-row-col)
+  (match (current-cursor)
+    [#f
+      (set! message "No primary cursor is visible") (set! cursor_row #f) (set! cursor_col #f)]
+    [(list pos kind) 
+      (set! cursor_row (position-row pos))
+      (set! cursor_col (position-col pos))]))
 
 
-(let [(mwbhome (maybe-get-env-var "WAKATIME_HOME")) (mbhome (maybe-get-env-var "HOME"))]
+(let [(mbwhome (maybe-get-env-var "WAKATIME_HOME")) (mbhome (maybe-get-env-var "HOME"))]
     (cond
         [(Ok? mbwhome) (set! home (unwrap-ok mbwhome))]
         [else (cond
-        [(Ok? mbhome)(set! home (unwrap-ok mbwhome))]
+        [(Ok? mbhome)(set! home (unwrap-ok mbhome))]
         [else (set! home "UNKNOWN")]
         )]
     )
@@ -28,5 +37,11 @@
 
 (define shared_state_parent_dir (string-append home "/.wakatime"))
 (define shared_state_file (string-append home "/helix_shared_state"))
+(define hb_last_sent (system-time/now))
+(define last_heartbeat (hash 'last_activity_at 0 'last_heartbeat_at 0 'file ""))
 
-(displayln waka.scm)
+(define wakatimecliversion spawn-process (run-shell-command "wakatime-cli" (list "--version")
+(echo wakatimecliversion)
+(if (not wakatimecliversion))) 
+[set! message "Wakatime cli not found!"]
+)
